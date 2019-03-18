4015fc90fb8d1a70c424c50a0ff5f4e517afa0c7
mike <redmike@mindspring.com>
2019-03-17 23:50:45 -0400

    Incremental development point 2

---
+++ app/src/main/java/com/example/android/tourguide/TourListEntryAdapter.java
@@ -0,0 +1,39 @@
+package com.example.android.tourguide;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TourListEntryAdapter extends ArrayAdapter<TourListEntry> {
+    public TourListEntryAdapter(Context context, int resource, ArrayList<TourListEntry> tourList) {
+        super(context, resource, tourList);
+    }
+
+    @BindView(R.id.titleName) TextView title;
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup container) {
+        TourListEntry entry = getItem(position);
+
+        // if a new list, inflate a new list item
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.tour_list_item_layout, container, false);
+        }
+
+        // call the butterknife binding method
+        ButterKnife.bind(this, convertView);
+
+        title.setText(entry.getName());
+
+        return convertView;
+    }
+}
